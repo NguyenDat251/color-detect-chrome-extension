56a3cd74a6b91f5a5ac7caa0abb544de8e6ebbd4
dat <daniel@gotitapp.co>
2023-02-22 00:52:39 +0700

    update taking screen shot

--- background.js
+++ background.js
@@ -56,43 +56,7 @@ chrome.action.onClicked.addListener(async (tab) => {
     target: { tabId: tab.id },
   });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["scripts/content.js"],
-  });
-
-  chrome.desktopCapture.chooseDesktopMedia(
-    ["screen", "window", "tab"],
-    tab,
-    (streamId) => {
-      //check whether the user canceled the request or not
-      if (streamId && streamId.length) {
-        //check whether the user canceled the request or not
-
-        setTimeout(() => {
-          chrome.tabs.sendMessage(
-            tab.id,
-            { name: "stream", streamId },
-            (response) => console.log(response)
-          );
-        }, 200);
-      }
-    }
-  );
-
-  chrome.runtime.onMessage.addListener((message, sender, senderResponse) => {
-    if (message.name === "download" && message.url) {
-      chrome.downloads.download(
-        {
-          filename: "screenshot.png",
-          url: message.url,
-        },
-        (downloadId) => {
-          senderResponse({ success: true });
-        }
-      );
-
-      return true;
-    }
+  chrome.tabs.captureVisibleTab(null, {}, function (image) {
+    chrome.tabs.sendMessage(tab.id, { image });
   });
 });
--- scripts/screenShot.js
+++ scripts/screenShot.js
@@ -6,67 +6,58 @@ function componentToHex(c) {
   return hex.length == 1 ? "0" + hex : hex;
 }
 
-chrome.runtime.onMessage.addListener((message, sender, senderResponse) => {
-  if (message.name === "stream" && message.streamId) {
-    let track, canvas;
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          mandatory: {
-            chromeMediaSource: "desktop",
-            chromeMediaSourceId: message.streamId,
-          },
-        },
-      })
-      .then((stream) => {
-        track = stream.getVideoTracks()[0];
-        const imageCapture = new ImageCapture(track);
-        return imageCapture.grabFrame();
-      })
-      .then((bitmap) => {
-        track.stop();
-        canvas = document.createElement("canvas");
-        canvas.width = bitmap.width;
-        canvas.height = bitmap.height;
-        let context = canvas.getContext("2d");
-        context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  const { image } = request;
+  const img = new Image();
 
-        document.body.appendChild(canvas);
+  const canvas = document.createElement("canvas");
+  let context = canvas.getContext("2d");
 
-        let actualColor = "ffffff";
+  img.src = image;
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0); // Or at whatever offset you like
+  };
 
-        // canvas.addEventListener("mousemove", function (e) {
-        //   var pos = {
-        //     x: canvas.offsetLeft,
-        //     y: canvas.offsetTop,
-        //   };
-        //   var x = e.pageX - pos.x;
-        //   var y = e.pageY - pos.y;
-        //   var c = canvas.getContext("2d");
-        //   var p = c.getImageData(x, y, 1, 1).data;
-        //   actualColor = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-        //   console.log("🚀 ~ file: screenShot.js:46 ~ actualColor", actualColor);
-        // });
+  canvas.classList.add("mainCanvas");
 
-        // return canvas.toDataURL();
-      })
-      //   //TODO download the image from the URL
-      //   .then((url) => {
-      //     chrome.runtime.sendMessage({ name: "download", url }, (response) => {
-      //       if (response.success) {
-      //         alert("Screenshot saved");
-      //       } else {
-      //         alert("Could not save screenshot");
-      //       }
-      //       canvas.remove();
-      //       senderResponse({ success: true });
-      //     });
-      //   })
-      .catch((err) => {
-        alert("Could not take screenshot");
-        senderResponse({ success: false, message: err });
-      });
+  document.body.appendChild(canvas);
 
-    return true;
-  }
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltip-color-detect");
+
+  const colorPreview = document.createElement("div");
+  colorPreview.classList.add("colorPreview-color-detect");
+
+  tooltip.appendChild(colorPreview);
+
+  const colorHexCode = document.createElement("span");
+  colorHexCode.classList.add("colorHexCode-color-detect");
+  colorHexCode.textContent = "#b3e879";
+
+  tooltip.appendChild(colorHexCode);
+
+  document.body.appendChild(tooltip);
+
+  canvas.addEventListener("mousemove", function (e) {
+    var pos = {
+      x: canvas.offsetLeft,
+      y: canvas.offsetTop,
+    };
+    var x = e.pageX - pos.x;
+    var y = e.pageY - pos.y;
+    var c = canvas.getContext("2d");
+    var p = c.getImageData(x, y, 1, 1).data;
+    actualColor = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+
+    colorPreview.style.backgroundColor = actualColor;
+    colorHexCode.textContent = actualColor;
+    let xPointer = e.clientX,
+      yPointer = e.clientY;
+    tooltip.style.top = yPointer - 90 + "px";
+    tooltip.style.left = xPointer + "px";
+  });
+
+  document.body.style.removeProperty("pointer-events");
 });
